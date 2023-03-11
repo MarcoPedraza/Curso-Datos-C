0fbf86e4208a7d31fc0471d8c4ddf1dc23000abe
MarcoPedraza <marco.pedraza.palacios@gmail.com>
2023-03-11 13:48:53 +0100

    Muchas mierdas que no entiendo

---
+++ masejercicospointers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main(){
+
+
+    int entero = 10, *apuntadorEntero = &entero;
+    float flotante = 2.5, *apuntadorFlotante =&flotante;
+    char caracter = 'h', *apuntadorCaracter = &caracter;
+
+   
+    printf("Entero (variable): %d\n",entero);
+    printf("Apuntador *: %d\n",*apuntadorEntero);
+    printf("Apunador entero memoria: %p\n",apuntadorEntero);
+
+    return 0;
+}
